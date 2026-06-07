5b4bb67eaef5b38dc25deb228d7b5294ab7d7e65
SHERY <sheryoiix@gmail.com>
2026-06-07 14:58:53 +0500

    Create index.ts to export BrainGym components

---
+++ src/components/BrainGym/src/components/BrainGym/src/components/BrainGym/src/components/BrainGym/src/components/BrainGym/index.ts
@@ -0,0 +1,5 @@
+export { BrainGymContainer } from './BrainGymContainer';
+export { ArithmeticSpeedRun } from './ArithmeticSpeedRun';
+export { MemoryMatrix } from './MemoryMatrix';
+export { AnagramSolver } from './AnagramSolver';
+export { PatternMaster } from './PatternMaster';
